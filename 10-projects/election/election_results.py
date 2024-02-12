82afa5bcb3895e9902b919bada6c0c6939ca572c
Caleb Kruse <caleb.krs@gmail.com>
2024-02-11 22:24:48 -0800

    reformat and move map below result

--- 10-projects/election/election_results.py
+++ 10-projects/election/election_results.py
@@ -6,26 +6,30 @@ import requests
 import streamlit as st
 from openai import OpenAI
 
+
 def parse_store_name(store_name):
     # Use OpenAI to convert the store name to the OSM name
     # For example, "whole foods" -> "Whole Foods Market"
 
-    client = OpenAI(api_key= st.secrets["OPENAI_KEY"])
+    client = OpenAI(api_key=st.secrets["OPENAI_KEY"])
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
-        response_format={ "type": "json_object" },
+        response_format={"type": "json_object"},
         messages=[
-            {"role": "system", "content": "You convert unstructured names of stores to their name on OpenStreetMap in JSON format."},
+            {
+                "role": "system",
+                "content": "You convert unstructured names of stores to their name on OpenStreetMap in JSON format.",
+            },
             {"role": "user", "content": "The OSM name for " + store_name + " is "},
-        ]
+        ],
     )
     try:
-        name = json.loads(response.choices[0].message.content)['osm_name']
+        name = json.loads(response.choices[0].message.content)["osm_name"]
         return name
     except:
         print(response.choices[0].message.content)
         return store_name
-    
+
 
 def get_locations(store_name):
     # Define the bounding box coordinates for the mainland US
@@ -66,15 +70,24 @@ def get_fips(lon, lat):
 
 
 # Load the fips data
-fips_center = pd.read_csv("https://raw.githubusercontent.com/clkruse/clkruse.github.io/master/10-projects/election/fips-county-center.csv", dtype={"fips_code": str})
-results = requests.get("https://raw.githubusercontent.com/clkruse/clkruse.github.io/master/10-projects/election/election_results_by_fips.json")
+fips_center = pd.read_csv(
+    "https://raw.githubusercontent.com/clkruse/clkruse.github.io/master/10-projects/election/fips-county-center.csv",
+    dtype={"fips_code": str},
+)
+results = requests.get(
+    "https://raw.githubusercontent.com/clkruse/clkruse.github.io/master/10-projects/election/election_results_by_fips.json"
+)
 # load the json data
 fips_results = json.loads(results.text)
 
 # create a title
 st.title("If stores could vote...")
-st.write("If you're standing in a Whole Foods, what are the odds that the county you're in voted for Biden? What about a Dollar General? I was curious, so I built a tool to find the election results for any store in the US.")
-st.write("Enter the name of a store and I'll show you how counties with that store voted. Note that stores are often built in cities, and cities tend to vote for Democrats. Anything that shows > 50% for Trump is generally an interesting finding.")
+st.write(
+    "If you're standing in a Whole Foods, what are the odds that the county you're in voted for Biden? What about a Dollar General? I was curious, so I built a tool to find the election results for any store in the US."
+)
+st.write(
+    "Enter the name of a store and I'll show you how counties with that store voted. Note that stores are often built in cities, and cities tend to vote for Democrats. Anything that shows > 50% for Trump is generally an interesting finding."
+)
 
 # create an input box to get the store name
 store_name = st.text_input("Enter a store name")
@@ -87,11 +100,7 @@ if store_name:
         st.write(f"No stores found with the name {store_name}")
     else:
         st.write(f"Found {len(locations)} stores with the name {store_name}")
-        # create a locations dataframe
         locations_df = pd.DataFrame(locations, columns=["lon", "lat"])
-        # create a map
-        st.map(locations_df)
-
         # get the fips code for each location
         fips_codes = []
         for i in range(len(locations)):
@@ -105,19 +114,27 @@ if store_name:
         results_df["FIPS"] = fips_codes
         results_df["Biden"] = [result["bidenj"] for result in results]
         results_df["Trump"] = [result["trumpd"] for result in results]
-        results_df["Winner"] = ["Biden" if result["bidenj"] > result["trumpd"] else "Trump" for result in results]
+        results_df["Winner"] = [
+            "Biden" if result["bidenj"] > result["trumpd"] else "Trump"
+            for result in results
+        ]
         # de-duplicate based on fips code
         results_df = results_df.drop_duplicates(subset="FIPS")
         # check if one candidate won all of the counties
         if len(results_df["Winner"].unique()) == 1:
-            st.write(f"All {len(results_df)} counties with a {store_name} voted for {results_df['Winner'].unique()[0]}")
+            st.write(
+                f"All {len(results_df)} counties with a {store_name} voted for {results_df['Winner'].unique()[0]}"
+            )
         else:
             biden_wins = results_df["Winner"].value_counts()["Biden"]
             trump_wins = results_df["Winner"].value_counts()["Trump"]
             # display how many stores voted for each candidate
-            st.write(f"{biden_wins:,} counties with a {store_name} voted for Biden ({biden_wins / (biden_wins + trump_wins):.1%}) and {trump_wins:,} voted for Trump ({trump_wins / (biden_wins + trump_wins):.1%})")
+            st.write(
+                f"{biden_wins:,} counties with a {store_name} voted for Biden ({biden_wins / (biden_wins + trump_wins):.1%}) and {trump_wins:,} voted for Trump ({trump_wins / (biden_wins + trump_wins):.1%})"
+            )
+        st.map(locations_df)
 
-footer="""<style>
+footer = """<style>
 .footer {
 position: fixed;
 left: 0;
@@ -132,4 +149,4 @@ text-align: center;
 <p>Made by <a href="https://calebkruse.com/" target="_blank">Caleb Kruse</a></p>
 </div>
 """
-st.markdown(footer,unsafe_allow_html=True)
+st.markdown(footer, unsafe_allow_html=True)
