de21d2137dc08d07359400a827081e595d2c913b
Caleb Kruse <caleb.krs@gmail.com>
2024-02-13 18:48:04 -0800

    bigger points

--- 10-projects/election/election_results.py
+++ 10-projects/election/election_results.py
@@ -150,7 +150,7 @@ if store_name:
                         "ScatterplotLayer",
                         data=results_df,
                         get_position=["lon", "lat"],
-                        get_radius=10000,
+                        get_radius=15000,
                         get_fill_color="color",
                     ),
                 ],
